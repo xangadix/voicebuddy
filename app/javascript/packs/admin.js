b3c452b7c39a9c30b38298ac73587a8658705ace
Daan Nolen <daan@develop.sense-studios.com>
2021-06-22 12:52:48 +0200

    lots of fixes, but I think we're gettiung there now

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,12 +1,13 @@
 window.onload = function() {
   // language dropdown
-  if document.getElementById('language_select') == null return;
-  
-  document.getElementById('language_select').onclick = function() {
-    if ( document.getElementById('language_dropdown').style.display == "inline-block" ) {
-      document.getElementById('language_dropdown').style.display = "none"
-    }else{
-      document.getElementById('language_dropdown').style.display = "inline-block"
-    }
-  }
+  //
+  //if document.getElementById('language_select') == null return;
+
+  //document.getElementById('language_select').onclick = function() {
+  //  if ( document.getElementById('language_dropdown').style.display == "inline-block" ) {
+  //    document.getElementById('language_dropdown').style.display = "none"
+  //  }else{
+  //    document.getElementById('language_dropdown').style.display = "inline-block"
+  //  }
+  //}
 }
