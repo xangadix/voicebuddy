fa17e9c7e2f67601fbceff9d1b3ab5addaea601e
Daan Nolen <daan@develop.sense-studios.com>
2023-08-14 14:26:24 +0200

    adding last sign in to users or logos

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,8 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :trackable
 
   ## Database authenticatable
   field :email,              type: String, default: ""
